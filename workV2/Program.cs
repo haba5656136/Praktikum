bb6d94e81aa850134efeb57e7ed1146a13d94fbd
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-26 18:46:36 +0700

    создал второй массив для заполнения его числами с первого массива кроме чисел меньше 8,отформатировал код

--- workV2/Program.cs
+++ workV2/Program.cs
@@ -3,11 +3,21 @@
 
 Console.WriteLine("введите размерность массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
+int minNumber = 8;
 
-int [] array = new int[n];
-
+int[] array = new int[n];
 for (int i = 1; i < array.Length; i++)
 {
-    array[i] = new Random().Next(1,100);
-    Console.WriteLine(array[i] + " ");
+    array[i] = new Random().Next(1, 100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int[] arrayNew = array;
+for (int j = 0; j < arrayNew.Length; j++)
+{
+    if (arrayNew[j] > minNumber)
+    {
+        Console.Write(arrayNew[j] + " ");
+    }
 }
