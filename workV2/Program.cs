a83081ce7c0651f39e953842a888c2d6590fc035
haba5656136 <94282796+haba5656136@users.noreply.github.com>
2021-12-26 18:15:38 +0700

    написал код заполнения массива зандомными числами от 1 до 100, размерность массива задаем с клавиатуры

--- workV2/Program.cs
+++ workV2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива целых чисел формирует массив из чисел, больших 8
+
+
+Console.WriteLine("введите размерность массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int[n];
+
+for (int i = 1; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,100);
+    Console.WriteLine(array[i] + " ");
+}
